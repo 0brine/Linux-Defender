afedb351b9f755dc46991dddf13611e5b5ca6f2d
dabrandi0 <dabrandi0@gmx.at>
2020-10-02 16:42:47 +0200

    addded all ports

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ def pscan(t, p):
     print('Target', t, '\nPort', p, end=' ')
     try:
         s.connect((t, p))
-        print('is open\n')
+        print('is open')
     except:
-        print('is closed\n')
+        print('is closed\n------------------------')
 
 
+pscan("10.24.17.6", 22)     #ssh
 pscan("10.24.17.6", 25)     #smtp
+pscan("10.24.17.6", 587)    #smtp
 pscan("www", 80)            #http
 pscan("10.24.17.6", 443)    #https
+pscan("10.24.17.6", 143)    #imap
+pscan("10.24.17.6", 993)    #imaps
+pscan("10.24.17.6", 110)    #pop3
+pscan("10.24.17.6", 995)    #pop3s
